4ee23a00b4b7da8916853dfa78aca97d3bc217cd
Kamil Żurek (kz305428) <kz305428@student.polsl.pl>
2025-07-14 16:31:27 +0200

    Dodanie grafiki

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
+import React from 'react';
+
 export default function Hero() {
     return (
         <section className="hero">
-            <h1>Hurtownia Szkła ATEK-GLASS</h1>
+            <div className="hero-content">
+                <h1>Hurtownia Szkła ATEK-GLASS</h1>
+                <div className="hero-buttons">
+                    <a href="#offer" className="button">Oferta</a>
+                    <a href="#contact" className="button">Kontakt</a>
+                </div>
+            </div>
         </section>
     );
 }
