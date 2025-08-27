6279932c5e9c92f12b5cae5d72e54902aec27361
Kamil Żurek (kz305428) <kz305428@student.polsl.pl>
2025-08-27 18:24:08 +0200

    Added Vercel Analitics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 AOS.init({
     duration: 800,
@@ -13,5 +15,7 @@ AOS.init({
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <App />
-    </StrictMode>,
+        <Analytics />
+        <SpeedInsights />
+    </StrictMode>
 )
